1cd27848ffa5f12c833afe431d2a3f2f69af1d4a
MathPrm <primotmathilde@gmail.com>
2025-06-09 22:27:39 +0200

    feat(mascotte): add animated mascotte on mobile format

--- src/components/Sebi.js
+++ src/components/Sebi.js
@@ -1,13 +1,79 @@
-import SebiImage from '../assets/images/sebi.svg'
+import React, { useState, useEffect, useRef } from 'react';
 import '../assets/styles/sebi.css';
+import eyesOpened from '../assets/images/mascotte_head_open_eyes.png';
+import eyesClosed from '../assets/images/mascotte_head_eyes_closed.png';
+import body1 from '../assets/images/mascotte_body_1.png';
+import body2 from '../assets/images/mascotte_body_2.png';
 
-const Sebi = ({ text }) => {
+const SebiAnimation = ({ text })  => {
+  const [headSrc, setHeadSrc] = useState(eyesOpened);
+  const [bodySrc, setBodySrc] = useState(body1);
+  const [headRotation, setHeadRotation] = useState(0);
+  const headRef = useRef(null);
+  const bodyRef = useRef(null);
+  const intervalRef = useRef(null);
+  
+  const animation = [
+    eyesOpened,
+    eyesClosed,
+    body1,
+    body2
+  ];
+
+  const startAnimation = () => {
+    let i = 0;
+    let j = 0;
+    
+    intervalRef.current = setInterval(() => {
+      setHeadSrc(animation[1]);
+      setHeadRotation(i % 2 === 0 ? 10 : -10);
+      setBodySrc(j % 2 === 0 ? animation[2] : animation[3]);
+      i++;
+      j++;
+    }, 400);
+  };
+
+  const stopAnimation = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      setHeadSrc(animation[0]);
+      setBodySrc(animation[2]);
+      setHeadRotation(0);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className="sebi">
-      <p className='bubble speech'>{text}</p>
-      <img src={SebiImage} alt="Sebi la gazelle" />
+     <div className="sebi">
+        <p className='bubble speech'>{text}</p>
+        <div id="container">
+          <img
+            ref={headRef}
+            src={headSrc}
+            alt="sebi la gazelle"
+            id="head"
+            style={{ transform: `rotate(${headRotation}deg)` }}
+            onMouseEnter={startAnimation}
+            onMouseLeave={stopAnimation}
+          />
+          <img
+            ref={bodyRef}
+            src={bodySrc}
+            alt="sebi la gazelle"
+            id="body"
+            onMouseEnter={startAnimation}
+            onMouseLeave={stopAnimation}
+          />
+        </div>
     </div>
   );
 };
 
-export default Sebi;
+export default SebiAnimation;
